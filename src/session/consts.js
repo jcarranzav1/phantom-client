56db6a0347c08cb0598a973303dcdd83177bf429
Juan Diego Carranza Vega <juan.carranza@smartpyme.pe>
2022-02-24 09:42:32 -0500

    Remove cloduinary variable

--- src/session/consts.js
+++ src/session/consts.js
@@ -1,5 +1,4 @@
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 export const STRIPE_KEY = import.meta.env.VITE_STRIPE_KEY;
 export const TOKEN_KEY = 'token';
 export const CART_KEY = 'cart';
