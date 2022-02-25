5e9a0f0dd4061ebc96040028b656c8ba8de848d1
Juan Diego Carranza Vega <juan.carranza@smartpyme.pe>
2022-02-24 23:13:29 -0500

    Redirect edit product button to update product

--- src/components/Products/CardPageProducts.jsx
+++ src/components/Products/CardPageProducts.jsx
@@ -126,7 +126,7 @@ export default function CardPageProducts({ photo, price, model, id }) {
       </ModelContainer>
       <Values>${price.toFixed(2)}</Values>
       <ButtonContainer>
-        <EditButton>
+        <EditButton onClick={() => navigate(`/admin/product/${id}`)}>
           <EditIcon>
             <path d="m15 5-1.41 1.41L18.17 11H2v2h16.17l-4.59 4.59L15 19l7-7-7-7z" />
           </EditIcon>
