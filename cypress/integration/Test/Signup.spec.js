f83e313c3f1c9126aefcc0e24ec21ba8340c63f1
Juan Diego Carranza Vega <juan.carranza@smartpyme.pe>
2022-02-25 08:52:13 -0500

    Add cypress

---
+++ cypress/integration/Test/Signup.spec.js
@@ -0,0 +1,7 @@
+describe('Sign Up', () => {
+  // eslint-disable-next-line jest/expect-expect
+  it('sign-up as user', () => {
+    cy.visit('https://pc-phantom.netlify.app/');
+    cy.get();
+  });
+});
